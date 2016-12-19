f91a0b9449aa08c227e0ce219b06ad6157ac92f6
Loran Mutafov <loranmutafov@gmail.com>
2016-12-19 15:48:51 +0000

    Add support for words starting with a capital, as well as ALL-CAPS words

--- tests/Amara/Varcon/TranslatorTest.php
+++ tests/Amara/Varcon/TranslatorTest.php
@@ -29,33 +29,37 @@ class TranslatorTest extends PHPUnit_Framework_TestCase
     {
         return [
             [
+                // Assert words starting with a capital are translated and their capitalisation is preserved
                 $questionable = Translator::QUESTIONABLE_IGNORE,
-                $british = 'My pyjama\'s colour is not as greyish as it looks',
-                $american = 'My pajama\'s color is not as grayish as it looks',
-                $canadian = 'My pyjama\'s colour is not as greyish as it looks',
-                $australian = 'My pyjama\'s colour is not as greyish as it looks',
-                $british_z = 'My pyjama\'s colour is not as greyish as it looks',
+                $british = 'My Pyjama\'s Colour is not as greyish as it looks',
+                $american = 'My Pajama\'s Color is not as grayish as it looks',
+                $canadian = 'My Pyjama\'s Colour is not as greyish as it looks',
+                $australian = 'My Pyjama\'s Colour is not as greyish as it looks',
+                $british_z = 'My Pyjama\'s Colour is not as greyish as it looks',
                 $variation = null,
             ],
             [
+                // Assert all-caps strings are being translated and kept all-caps
                 $questionable = Translator::QUESTIONABLE_IGNORE,
-                $british = 'The 50x50centimetres-cabinet is finally finalised',
-                $american = 'The 50x50centimeters-cabinet is finally finalized',
-                $canadian = 'The 50x50centimetres-cabinet is finally finalized',
-                $australian = 'The 50x50centimetres-cabinet is finally finalised',
-                $british_z = 'The 50x50centimetres-cabinet is finally finalized',
+                $british = 'The 50x50centimetres-cabinet is finally FINALISED',
+                $american = 'The 50x50centimeters-cabinet is finally FINALIZED',
+                $canadian = 'The 50x50centimetres-cabinet is finally FINALIZED',
+                $australian = 'The 50x50centimetres-cabinet is finally FINALISED',
+                $british_z = 'The 50x50centimetres-cabinet is finally FINALIZED',
                 $variation = null,
             ],
             [
+                // Assert weird capitalisation is not translated
                 $questionable = Translator::QUESTIONABLE_IGNORE,
-                $british = 'No uncommon words here',
-                $american = 'No uncommon words here',
-                $canadian = 'No uncommon words here',
-                $australian = 'No uncommon words here',
-                $british_z = 'No uncommon words here',
-                $variation = 'No uncommon words here',
+                $british = 'The quick GraY cat is gREy',
+                $american = 'The quick GraY cat is gREy',
+                $canadian = 'The quick GraY cat is gREy',
+                $australian = 'The quick GraY cat is gREy',
+                $british_z = 'The quick GraY cat is gREy',
+                $variation = 'The quick GraY cat is gREy',
             ],
             [
+                // Assert questionable translations are used when the include flag is set
                 $questionable = Translator::QUESTIONABLE_INCLUDE,
                 $british = ['One metre', 'One meter'],
                 $american = 'One meter',
@@ -65,6 +69,7 @@ class TranslatorTest extends PHPUnit_Framework_TestCase
                 $variation = null,
             ],
             [
+                // Assert questionable translations are used when the include flag is set
                 $questionable = Translator::QUESTIONABLE_INCLUDE,
                 $british = ['adviser', 'advisor'],
                 $american = ['adviser', 'advisor'],
@@ -74,6 +79,7 @@ class TranslatorTest extends PHPUnit_Framework_TestCase
                 $variation = ['adviser', 'advisor'],
             ],
             [
+                // Assert questionable translations are marked when the mark flag is set
                 $questionable = Translator::QUESTIONABLE_MARK,
                 $british = ['One metre high', 'One ?meter/metre? high'],
                 $american = 'One meter high',
